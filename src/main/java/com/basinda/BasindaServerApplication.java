ef8454ee5752ccd861ff17aa3f6eb031f26aea23
md.farid <md.farid.ice@gmail.com>
2023-06-16 21:16:42 +0600

    startup project.

---
+++ src/main/java/com/basinda/BasindaServerApplication.java
@@ -0,0 +1,13 @@
+package com.basinda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasindaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasindaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basinda/BasindaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basinda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasindaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
