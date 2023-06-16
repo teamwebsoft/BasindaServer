8197cdce3f3e777cecfe9f228032dd7b5e0fcd1a
md.farid <md.farid.ice@gmail.com>
2023-06-16 22:42:25 +0600

    logger converter and utils created.

---
+++ src/main/java/com/basinda/LogUtil.java
@@ -0,0 +1,60 @@
+package com.basinda;
+
+import com.basinda.converter.GsonConverter;
+import org.slf4j.Logger;
+
+public class LogUtil {
+    private static final String EXCEPTION_LOG = "GsonConverter.ObjectToJson has Exception : ";
+
+    private LogUtil(){
+
+    }
+
+    public static String GetObjectName(Object object){
+        if (object == null) return "";
+        String objectName = object.getClass().getName();
+        return objectName.substring(objectName.lastIndexOf('.')+1);
+    }
+
+    public static void printDebug(Logger logger, Object object){
+        String log = "";
+        try {
+            log += GsonConverter.ObjectToJson(object);
+        }catch (Exception ex){
+            logger.error(EXCEPTION_LOG, ex);
+        }
+        logger.debug(log);
+    }
+
+    public static void PrintObject(Logger logger, Object object){
+        String log = GetObjectName(object);
+        try {
+            log += GsonConverter.ObjectToJson(object);
+        }catch (Exception ex){
+            logger.error(EXCEPTION_LOG, ex);
+        }
+        logger.debug(log);
+    }
+
+    public static void PrintObject(Logger logger, Object object, String description){
+        String log = GetObjectName(object) + " - "+description;
+        try {
+            log += GsonConverter.ObjectToJson(object);
+        }catch (Exception ex){
+            logger.error(EXCEPTION_LOG, ex);
+        }
+        logger.debug(log);
+    }
+
+    public static void PrintObject(Logger logger, Object object, String description, boolean printContents){
+        String log = GetObjectName(object) + " - " +description;
+        if (printContents == true){
+            try {
+                log += GsonConverter.ObjectToJson(object);
+            }catch (Exception ex){
+                logger.error(EXCEPTION_LOG, ex);
+            }
+        }
+        logger.debug(log);
+    }
+}
