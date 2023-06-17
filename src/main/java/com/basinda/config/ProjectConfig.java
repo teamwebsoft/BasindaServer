96d7567ae5542dd02e7689a1646c2d1c8c73b0c4
md.farid <md.farid.ice@gmail.com>
2023-06-17 10:02:09 +0600

    Project configuration created.

---
+++ src/main/java/com/basinda/config/ProjectConfig.java
@@ -0,0 +1,14 @@
+package com.basinda.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
