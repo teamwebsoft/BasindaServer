2fdc08ee8ae0bb5d0388427cbc4bac02779b55e9
md.farid <md.farid.ice@gmail.com>
2023-06-18 17:11:37 +0600

    Division, District, Pourosova request created.

---
+++ src/main/java/com/basinda/requests/DistrictRequest.java
@@ -0,0 +1,11 @@
+package com.basinda.requests;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class DistrictRequest {
+    private String name;
+    private Long divisionId;
+}
---
+++ src/main/java/com/basinda/requests/PourosovaRequest.java
@@ -0,0 +1,11 @@
+package com.basinda.requests;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class PourosovaRequest {
+    private String name;
+    private Long districtId;
+}
