330e0e6693e84adf827a13ff989994cd6769ec97
md.farid <md.farid.ice@gmail.com>
2023-06-20 22:05:25 +0600

    code refactor.

--- src/main/java/com/basinda/controllers/UserController.java
+++ src/main/java/com/basinda/controllers/UserController.java
@@ -1,12 +1,12 @@
 package com.basinda.controllers;
 
 import com.basinda.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 public class UserController {
