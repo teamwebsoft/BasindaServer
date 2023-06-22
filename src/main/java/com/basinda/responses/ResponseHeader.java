14e690790b43982ab5da987f9b69daa065a0ebf6
md.farid <md.farid.ice@gmail.com>
2023-06-22 21:37:01 +0600

    Response Header class created.

---
+++ src/main/java/com/basinda/responses/ResponseHeader.java
@@ -0,0 +1,9 @@
+package com.basinda.responses;
+
+import org.springframework.http.HttpStatus;
+
+public class ResponseHeader {
+    public boolean status;
+    public String content;
+    public HttpStatus statusCode;
+}
