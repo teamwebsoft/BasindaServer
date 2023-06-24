24ecfd44bd739fe4836c5213704549ce5eb833db
md.farid <md.farid.ice@gmail.com>
2023-06-24 22:59:34 +0600

    cors related change and jwt configuration.

--- src/main/java/com/basinda/config/ProjectSecurityConfig.java
+++ src/main/java/com/basinda/config/ProjectSecurityConfig.java
@@ -1,11 +1,18 @@
 package com.basinda.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -13,7 +20,20 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         return http
-                .cors().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .cors().configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration configuration = new CorsConfiguration();
+                        configuration.setAllowCredentials(true);
+                        configuration.setAllowedOrigins(Collections.singletonList("*"));
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setExposedHeaders(Arrays.asList("Authorization"));
+                        configuration.setMaxAge(3600L);
+                        return configuration;
+                    }
+                }).and()
                 .csrf().disable()
                 .authorizeHttpRequests().anyRequest().permitAll()
                 //.requestMatchers("/actuator","/auth/register","/auth/login").permitAll()
