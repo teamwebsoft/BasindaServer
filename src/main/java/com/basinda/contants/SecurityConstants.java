e7117e2f91c912c79ed93bdeea1a392207b4be55
md.farid <md.farid.ice@gmail.com>
2023-06-25 00:41:19 +0600

    constant created.

---
+++ src/main/java/com/basinda/contants/SecurityConstants.java
@@ -0,0 +1,6 @@
+package com.basinda.contants;
+
+public class SecurityConstants {
+    public static final String JWT_HEADER = "Authorization";
+    public static final String JWT_KEY = "jxgEQeXHuPq8VdbyYFNkANdudQ3YUn4";
+}
