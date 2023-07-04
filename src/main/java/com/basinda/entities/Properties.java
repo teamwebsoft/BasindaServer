4cb3713c72c7897936b75053b8a067ee7b334648
md.farid <md.farid.ice@gmail.com>
2023-07-04 20:52:52 +0600

    property entity created.

---
+++ src/main/java/com/basinda/entities/Properties.java
@@ -0,0 +1,17 @@
+package com.basinda.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "properties")
+@Setter
+@Getter
+public class Properties {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String key;
+    private String value;
+}
