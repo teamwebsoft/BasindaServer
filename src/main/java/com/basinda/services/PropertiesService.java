9176421f57625e44238427a1dc22114c374e75fe
md.farid <md.farid.ice@gmail.com>
2023-07-04 20:57:42 +0600

    properties service and its implementation class created.

---
+++ src/main/java/com/basinda/services/PropertiesService.java
@@ -0,0 +1,5 @@
+package com.basinda.services;
+
+public interface PropertiesService {
+
+}
---
+++ src/main/java/com/basinda/services/impl/PropertiesServiceImpl.java
@@ -0,0 +1,9 @@
+package com.basinda.services.impl;
+
+import com.basinda.services.PropertiesService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PropertiesServiceImpl implements PropertiesService {
+
+}
