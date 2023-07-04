963f0e7746a9af202f00a999dd88ab1493369f9f
md.farid <md.farid.ice@gmail.com>
2023-07-04 21:02:53 +0600

    properties controller created.

---
+++ src/main/java/com/basinda/controllers/PropertiesController.java
@@ -0,0 +1,20 @@
+package com.basinda.controllers;
+
+import org.springframework.http.HttpStatus;
+import com.basinda.services.PropertiesService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RestController
+public class PropertiesController {
+
+    @Autowired
+    private PropertiesService propertiesService;
+
+    @PostMapping("/properties")
+    public ResponseEntity<String> propertiesSave(){
+        return ResponseEntity.status(HttpStatus.CREATED).body("Done");
+    }
+}
