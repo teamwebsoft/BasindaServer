9da497751ec768c1fe1378750c5cc2ceba840cd3
md.farid <md.farid.ice@gmail.com>
2023-07-08 16:33:15 +0600

    profession service and implementation class created.

---
+++ src/main/java/com/basinda/services/impl/ProfessionServiceImpl.java
@@ -0,0 +1,9 @@
+package com.basinda.services.impl;
+
+import com.basinda.services.ProfessionService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProfessionServiceImpl implements ProfessionService {
+
+}
